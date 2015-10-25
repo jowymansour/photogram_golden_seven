fe1511d5b5056ebbbf1767f40dc8d7c6a354106d
jordi92500 <jordi92500@hotmail.fr>
2015-10-24 20:13:27 -0500

    Rest of the CRUD fixed

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -17,13 +17,37 @@ class PhotosController < ApplicationController
 
     photo = Photo.new(source: photo_image_url, caption: photo_caption )
     if photo.save
-      flash[:danger] = "Your photo has been succesfully added."
+      flash[:success] = "Your photo has been succesfully added."
       redirect_to 'http://localhost:3000/photos'
     else
       flash[:danger] = "Please enter an image URL to continue"
       render 'new_form'
     end
 
+  end
+
+  def destroy
+    Photo.find(params[:id]).destroy
+    flash[:success] = "The photo is succesfully deleted"
+    redirect_to 'http://localhost:3000/photos'
+  end
+
+  def edit_form
+    photo_id = params[:id]
+    @photo = Photo.find_by({ :id => photo_id })
+  end
+
+  def update_row
+    @photo = Photo.find_by({ :id => params[:id] })
+    @photo.caption = params["the_caption"]
+    @photo.source = params["the_source"]
+    if @photo.save
+      flash[:success] = "Your photo has been succesfully updated."
+      render 'show'
+    else
+      flash[:danger] = "Please enter an image URL to continue"
+      render 'edit_form'
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
-  # Routes to READ photos
+  # Routes to ressources: photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/new",       { :controller => "photos", :action => "new_form" })
   get("/create_photo",     { :controller => "photos", :action => "create_row" })
+  get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
+  get("/photos/:id/edit",  { :controller => "photos", :action => "edit_form" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+  get("/update_photo/:id", { :controller => "photos", :action => "update_row" })
 
 
 end
