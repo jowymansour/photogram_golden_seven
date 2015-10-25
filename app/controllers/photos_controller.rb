e8cd559268af8d29246602814693e65fdae9d9e0
jordi92500 <jordi92500@hotmail.fr>
2015-10-24 19:43:24 -0500

    Create fix (+ error message if no URL)

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -8,4 +8,22 @@ class PhotosController < ApplicationController
     @photo = Photo.find_by({ :id => photo_id })
   end
 
+  def new_form
+  end
+
+  def create_row
+    photo_image_url = params["the_source"]
+    photo_caption = params["the_caption"]
+
+    photo = Photo.new(source: photo_image_url, caption: photo_caption )
+    if photo.save
+      flash[:danger] = "Your photo has been succesfully added."
+      redirect_to 'http://localhost:3000/photos'
+    else
+      flash[:danger] = "Please enter an image URL to continue"
+      render 'new_form'
+    end
+
+  end
+
 end
